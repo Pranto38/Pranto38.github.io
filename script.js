14e352531379a4e1e1c7ac2bc1783f19454ced52
Pranto38 <73888941+Pranto38@users.noreply.github.com>
2020-11-06 15:12:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "p" && b=="83")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
