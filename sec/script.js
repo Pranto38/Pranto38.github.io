dc8c41dbf6fab787582b3aa1131082724fc0dcb4
Pranto <73888941+Pranto38@users.noreply.github.com>
2020-11-24 18:36:02 +0600

    Add files via upload

---
+++ sec/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter password");
+  	if(a== "sai" && b=="1902110")
+  	{
+  	    window.location.href="index-particles.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="sai.html";
+  	}
+  	
