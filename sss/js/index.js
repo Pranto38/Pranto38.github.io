98b9fee75ddf94dc1475526793952787e2799f32
Pranto <1902134@student.ruet.ac.bd>
2023-11-08 06:41:59 +0600

    Update index.js

--- sss/js/index.js
+++ sss/js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/0h1hHOMPRuzw?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "HBD.mp4");
  https://youtu.be/orW_IF12BgI?t=1
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
